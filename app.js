cd32a14c09afea13bbdb8a1353e1d6f0e5d73cc5
coopwork <cooperyan66@gmail.com>
2022-09-19 21:14:03 +0600

    testiing popups and interactive functional

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-const preview_URL = document.querySelector('link[rel="image_src"]').getAttribute('href');
+
+// const preview_URL2 = document.querySelector('link[rel="image_src"]').getAttribute('href'),
+//         buttonGet = document.querySelector('.get_button');
+const preview_URL = 'https://i.ytimg.com/vi/uV4L-wcnK3Y/maxresdefault.jpg';
+
+// button_GET.addEventListener('click', () => {
+//     window.open(preview_URL);
+// })
+
+// function openFullPreview() {
+//     console.log('action: ', preview_URL);
+//     window.open(preview_URL);
+//     window.location(preview_URL);
+// }
 window.open(preview_URL);
-console.log(document.querySelector('link[rel="image_src"]').getAttribute('href'));
+
+
+// window.open(preview_URL);
+
+// console.log(document.querySelector('.image_wrapper'));
+// document.querySelector('.image_wrapper img').setAttribute('src', preview_URL);
+// console.log('img src',  document.querySelector('.get_youtube_preview_menu .image_wrapper img').getAttribute('src'));
+
+
+console.log('work: ', preview_URL);
+setInterval(() => {
+    console.log('work: ', preview_URL);
+}, 1000);
+
